84b587f5a5805f7407180cb4c4055a6f57ee103f
Albert Fotso <legrandpone1@gmail.com>
2025-12-20 22:25:54 +0100

    Ajout des balise des variables de seo pour les modification dans les vues

--- home/views/network.py
+++ home/views/network.py
@@ -4,8 +4,9 @@ from django.views import View
 
 class NetworkView(View):
     def get(self, request):
-        return render_inertia(request, "home/Network")
+        return render_inertia(request, "home/Network", None, {
+            "seo_title" : "ENSPM Hub - Réseau des Alumni, Étudiants et Partenaires",
+            "seo_description" : "ENSPM Hub - Plateforme de mise en relation des alumni, étudiants et partenaires de l'École Nationale Supérieure Polytechnique de Maroua."
+        })
 
-def network(request):
-    return render_inertia(request, "home/Network")
 
--- home/views/opportunites.py
+++ home/views/opportunites.py
@@ -3,6 +3,9 @@ from django.views import View
 
 class OpportunitiesView(View):
     def get(self, request):
-        return render_inertia(request, "home/Opportunities")
+        return render_inertia(request, "home/Opportunities", None, {
+            "seo_title" : "ENSPM Hub - Opportunités",
+            "seo_description": "ENSPM Hub - Reseau d'opportnuites de formation et emploi."
+        })
 
 
